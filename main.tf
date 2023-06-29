edfd0c8e5776dc73bb1698db682fc7ce8a50054e
yegorovev <91023323+yegorovev@users.noreply.github.com>
2023-06-29 18:47:17 +0400

    Add encryption (#2)

--- main.tf
+++ main.tf
@@ -13,6 +13,16 @@ resource "aws_s3_bucket" "terraform_state" {
   }
 }
 
+resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
 resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
   bucket = aws_s3_bucket.terraform_state.id
   versioning_configuration {
